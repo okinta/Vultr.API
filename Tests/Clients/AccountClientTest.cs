224e44484e743b2dbc40d1a9825156e330b692b8
Robert Roose <robert@shivercube.com>
2020-05-12 13:16:48 -0300

    Began adding tests

---
+++ Tests/Clients/AccountClientTest.cs
@@ -0,0 +1,23 @@
+﻿using System.Configuration;
+using Vultr.API;
+using Xunit;
+
+namespace Tests.Clients
+{
+    public class AccountClientTest
+    {
+        private static string ApiKey
+        {
+            get { return ConfigurationManager.AppSettings["ApiKey"]; }
+        }
+
+        [Fact]
+        public void TestGetInfo()
+        {
+            var client = new VultrClient(ApiKey);
+            var account = client.Account.GetInfo();
+
+            Assert.True(int.TryParse(account.Account.Balance, out int _));
+        }
+    }
+}
