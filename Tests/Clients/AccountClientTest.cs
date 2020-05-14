e6e0b22acac8397d26bd83af64f84e1cb1775649
Robert Roose <robert@shivercube.com>
2020-05-14 19:34:07 -0300

    Added a mock HTTP test for AccountClient

--- Tests/Clients/AccountClientTest.cs
+++ Tests/Clients/AccountClientTest.cs
@@ -1,10 +1,18 @@
-﻿using Vultr.API;
+﻿using MockHttpServer;
+using System.Collections.Generic;
+using Tests.Properties;
+using Vultr.API;
 using Xunit;
 
 namespace Tests.Clients
 {
     public class AccountClientTest
     {
+        private const int TestPort = 8451;
+        private const string TestURL = "http://localhost:8451/";
+
+        protected VultrClient Client { get; } = new VultrClient("abc123", TestURL);
+
         [Fact]
         public void TestGetInfo()
         {
@@ -12,6 +20,23 @@ namespace Tests.Clients
             var account = client.Account.GetInfo();
 
             Assert.True(float.TryParse(account.Account.balance, out float _));
+        }
+
+        [Fact]
+        public void TestMockGetInfo()
+        {
+            var requestHandlers = new List<MockHttpHandler>()
+            {
+                new MockHttpHandler("/account/info", "GET", (req, rsp, prm) =>
+                    Resources.AccountInfo)
+            };
+
+            using (new MockServer(TestPort, requestHandlers))
+            {
+                var account = Client.Account.GetInfo();
+
+                Assert.Equal("-5519.11", account.Account.balance);
+            }
         }
     }
 }
